3be8465cf33964bb35bcd241ace7eb0b49dbfca2
dchooyc <dchooyc@gmail.com>
2020-09-23 02:27:43 +0800

    Player 1 can now pick a position

--- ttt.js
+++ ttt.js
@@ -7,32 +7,52 @@ const rl = readline.createInterface({
 });
 
 var array = [];
+var gridBy = 0;
 
 rl.question("Player 1 please enter your name: ", function(player1) {
     rl.question("Player 2 please enter your name: ", function(player2) {
         rl.question("Choose a number from 3-9: ", function(number) {
         console.log(`${player1} you will be using the 'x'\n${player2} you will be using the 'o'\n`);
         console.log(`Your tic tac toe grid will be a ${number} by ${number}`);
-        console.log(gridConstructor(number));
-        rl.close();
+        gridBy = parseInt(number, 10);
+        populateArray(gridBy);
+        console.log(gridConstructor(gridBy));
+        playerOneTurn();
         });
     });
 });
 
-function gridConstructor(gridLength) {   
+function playerOneTurn() {
+    rl.question("Player 1 please pick a position: ", function(position) {
+        var xPos = parseInt(position, 10) - 1;
+        array[xPos] = " x";
+        console.log(gridConstructor(gridBy));
+        rl.close();
+    });
+}
+
+function populateArray(number) {
+    //Push the number of positions into the array
+    for (let i = 0; i<(number)*(number); i ++) {
+        arrayInput ="";
+        numberInput = i + 1;
+        if (i < 9) {
+            arrayInput += " ";
+        }
+        //Can only do this in js I think
+        arrayInput += numberInput;
+        array.push(arrayInput);
+    }
+}
+
+function gridConstructor(number) {   
     //Dash is the number of dashes needed
-    var number = parseInt(gridLength, 10);
     var dash   = (number*5)-1;
     var dashString = "";
 
     //Create the number of dashes
     for (let i = 0; i<dash; i++) {
         dashString += "-";
-    }
-
-    //Push the number of positions into the array
-    for (let i = 0; i<(number)*(number); i ++) {
-        array.push(i+1);
     }
 
     var string = "";
@@ -47,10 +67,6 @@ function gridConstructor(gridLength) {
             //Only the first digit doesn't have a | before it
             if (j > 0) {
                 string += "|";
-            }
-            //If the number to insert into the grid is single digit add an extra space
-            if ((array[j+(i*(number))]) < 10) {
-            string += " ";
             }
             string += " " + array[j+(i*(number))] + " ";
         }
