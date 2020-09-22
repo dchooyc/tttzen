92f11b54d200ce55ba1ed6cf67bd4cf7613ea178
dchooyc <dchooyc@gmail.com>
2020-09-23 00:12:26 +0800

    Outputs a perfect grid!

--- ttt.js
+++ ttt.js
@@ -13,30 +13,44 @@ rl.question("Player 1 please enter your name: ", function(player1) {
         rl.question("Choose a number from 3-9: ", function(number) {
         console.log(`${player1} you will be using the 'x'\n${player2} you will be using the 'o'\n`);
         console.log(`Your tic tac toe grid will be a ${number} by ${number}`);
-
+        
+        //Dash is the number of dashes needed
         var number = parseInt(`${number}`, 10);
+        var dash   = (number*5)-1;
+        var dashString = "";
+        
+        //Create the number of dashes
+        for (i = 0; i<dash; i++) {
+            dashString += "-";
+        }
 
+        //Push the number of positions into the array
         for (i = 0; i<(number)*(number); i ++) {
-            array.push((i+1).toString());
+            array.push(i+1);
         }
         
         var string = "";
 
+        //Create the grid
         for (i = 0; i<(number); i++) {
+            //Only first line doesn't have dotted dashes above it
             if (i > 0) {
-                string += "\n-----------\n";
+                string += "\n" + dashString + "\n";
             }
             for (j = 0; j<(number); j++) {
+                //Only the first digit doesn't have a | before it
                 if (j > 0) {
                     string += "|";
+                }
+                //If the number to insert into the grid is single digit add an extra space
+                if ((array[j+(i*(number))]) < 10) {
+                string += " ";
                 }
                 string += " " + array[j+(i*(number))] + " ";
             }
         }
 
         console.log(string);
-
-        console.log(array);
         rl.close();
         });
     });
