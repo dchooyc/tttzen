df093c894ea8cd5265fd8a573a2a0d49db2e786c
dchooyc <dchooyc@gmail.com>
2020-09-23 20:10:49 +0800

    grid displays upto three digit positions

--- ttt.js
+++ ttt.js
@@ -5,6 +5,7 @@ const rl = readline.createInterface({
 });
 
 var array = [];
+var checkArray = [];
 var gridBy = 0;
 var turn = 0;
 var maxTurns = 0;
@@ -30,15 +31,21 @@ function receiveNames() {
 }
 
 function receiveNumber() {
-    rl.question("Choose a number from 3-9: ", function(number) {
+    rl.question("Choose a number from 3-25: ", function(number) {
         console.log("Your tic tac toe grid will be a " + number + " by " + number);
         gridBy = parseInt(number, 10);
         maxTurns = gridBy * gridBy;
         populateArray(gridBy);
+        populateCheckArray(gridBy);
         console.log(gridConstructor(gridBy));
         playerOneTurn();
     });
 }
+
+// function updateCheckArray(position, piece) {
+
+
+// }
 
 function playerOneTurn() {
     if (turn == maxTurns) {
@@ -46,7 +53,11 @@ function playerOneTurn() {
     }
     rl.question(p1 + " please pick a position: ", function(position) {
         var xPos = parseInt(position, 10) - 1;
-        array[xPos] = " x";
+        var text = "";
+        if (gridBy > 9) {
+            text += " ";
+        }
+        array[xPos] = text + " x";
         console.log(gridConstructor(gridBy));
         turn ++;
         playerTwoTurn();
@@ -59,7 +70,11 @@ function playerTwoTurn() {
     }
     rl.question(p2 + " please pick a position: ", function(position) {
         var oPos = parseInt(position, 10) - 1;
-        array[oPos] = " o";
+        var text = "";
+        if (gridBy > 9) {
+            text += " ";
+        }
+        array[oPos] = text + " o";
         console.log(gridConstructor(gridBy));
         turn ++;
         playerOneTurn();
@@ -74,10 +89,16 @@ function turnsOver() {
 function populateArray(number) {
     //Push the number of positions into the array
     for (let i = 0; i<(number)*(number); i ++) {
-        arrayInput ="";
+        arrayInput = "";
         numberInput = i + 1;
         if (i < 9) {
             arrayInput += " ";
+        }
+        //If displaying three digit numbers add an extra space for two digit numbers
+        if (number > 9) {
+            if (i < 99) {
+                arrayInput += " ";
+            }
         }
         //Can only do this in js I think
         arrayInput += numberInput;
@@ -85,9 +106,29 @@ function populateArray(number) {
     }
 }
 
+function populateCheckArray(number) {
+    for (let i = 0; i < number; i++) {
+        var tempArray = [];
+        for (let j = 0; j < number; j++) {
+            tempArray.push("n");
+        }
+        checkArray.push(tempArray);
+    }
+}
+
+//To add extra dash when displaying 3 digits
+function dashPerNum(number) {
+    var dpn = 5;
+    if (number > 9) {
+        dpn ++;
+    }
+    return dpn;
+}
+
 function gridConstructor(number) {   
     //Dash is the number of dashes needed
-    var dash   = (number*5)-1;
+    var dpn = dashPerNum(number);
+    var dash   = (number*dpn)-1;
     var dashString = "";
 
     //Create the number of dashes
